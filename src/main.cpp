963af10631a81f3a1bae5eb23cc0796db5cc3a5f
Miarot <zinchenkodan@gmail.com>
2023-06-10 18:08:37 +0300

    register and create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,121 @@
+#define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-int wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdSho) {
+#if defined(CreateWindow)
+#undef CreateWindow
+#endif // CreateWindow
+
+#if defined(max)
+#undef max
+#endif
+
+#if defined(min)
+#undef min
+#endif
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dx12.h>
+
+#include <cassert>
+#include <algorithm>
+
+HWND g_windowHandl;
+uint32_t g_width = 1080;
+uint32_t g_height = 720;
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+void RegisterWindowClass(HINSTANCE hInstance, const wchar_t * className) {
+	WNDCLASSEXW windowDesc = {};
+
+	windowDesc.cbSize = sizeof(WNDCLASSEXW);
+	windowDesc.style = CS_HREDRAW | CS_VREDRAW;
+	windowDesc.lpfnWndProc = &WndProc;
+	windowDesc.cbClsExtra = 0;
+	windowDesc.cbWndExtra = 0;
+	windowDesc.hInstance = hInstance;
+	windowDesc.hIcon = ::LoadIcon(hInstance, NULL);
+	windowDesc.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	windowDesc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	windowDesc.lpszMenuName = NULL;
+	windowDesc.lpszClassName = className;
+	windowDesc.hIconSm = ::LoadIcon(hInstance, NULL);
+
+	ATOM atom = ::RegisterClassExW(&windowDesc);
+
+	assert(atom > 0 && "Can`t register window class");
+}
+
+HWND CreateWindow(
+	HINSTANCE hInstance, 
+	const wchar_t * className, 
+	const wchar_t * windowName,
+	uint32_t width, 
+	uint32_t height) 
+{
+	RECT windowRect{0, 0, static_cast<LONG>(width), static_cast<LONG>(height)};
+	::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	int windowWidth = windowRect.right - windowRect.left;
+	int windowHeight = windowRect.bottom - windowRect.top;
+
+	int systemWidth = ::GetSystemMetrics(SM_CXSCREEN);
+	int systemHeight = ::GetSystemMetrics(SM_CYSCREEN);
+	
+	int windowX = std::max(0, (systemWidth - windowWidth) / 2);
+	int windowY = std::max(0, (systemHeight - windowHeight) / 2);
+
+	HWND windowHandl = ::CreateWindowExW(
+		NULL,
+		className,
+		windowName,
+		WS_OVERLAPPEDWINDOW,
+		windowX,
+		windowY,
+		windowWidth,
+		windowHeight,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	assert(windowHandl && "Cant`t create window");
+
+	return windowHandl;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message)
+	{
+	case WM_DESTROY:
+		::PostQuitMessage(0);
+		break;
+	default:
+		return ::DefWindowProcW(hwnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdSho) {
+	SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
+
+	const wchar_t * className = L"MainWindowClass";
+	RegisterWindowClass(hInstance, className);
+	g_windowHandl = CreateWindow(hInstance, className, L"Empty window", g_width, g_height);
+
+	::ShowWindow(g_windowHandl, SW_SHOW);
+
+	MSG msg{};
+
+	while (msg.message != WM_QUIT) {
+		if (::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+		}
+	}
+
 	return 0;
 }
