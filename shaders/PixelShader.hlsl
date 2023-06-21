2445fd12e041b40837f92e7bcab63be9fd8da128
Miarot <zinchenkodan@gmail.com>
2023-06-21 15:41:48 +0300

    fix shaders

--- shaders/PixelShader.hlsl
+++ shaders/PixelShader.hlsl
@@ -1,29 +1,9 @@
-struct VertexIn {
-    float3 position : POSITIONT;
-    float3 color : COLOR;
-};
-
-struct VertexOut {
+struct PixelIn
+{
     float4 color : COLOR;
-    float4 position : SV_Position;
 };
 
-struct ModelViewProjection {
-    matrix MVP;
-};
-
-ConstantBuffer<ModelViewProjection> ModelViewProjectionCB : register(b0);
-
-// Another way to define mvp gMVP is global name of matrix 
-//cbuffer cbPerObject : register(b0) {
-//    matrix gMVP;
-//}
-
-VertexOut main(VertexIn vin) {
-    VertexOut vout;
-    
-    vout.color = float4(vin.color, 1.0f);
-    vout.position =  mul(float4(vin.position, 1.0f), ModelViewProjectionCB.MVP);
-    
-    return vout;
+float4 main(PixelIn pin) : SV_Target
+{
+    return pin.color;
 }
--- shaders/VertexShader.hlsl
+++ shaders/VertexShader.hlsl
@@ -1,7 +1,33 @@
-struct PixelIn {
-    float4 color : COLOR;
+struct VertexIn
+{
+    float3 position : POSITIONT;
+    float3 color : COLOR;
 };
 
-float4 main(PixelIn pin) : SV_Target {
-    return pin.color;
+struct VertexOut
+{
+    float4 color : COLOR;
+    float4 position : SV_Position;
+};
+
+struct ModelViewProjection
+{
+    matrix MVP;
+};
+
+ConstantBuffer<ModelViewProjection> ModelViewProjectionCB : register(b0);
+
+// Another way to define mvp gMVP is global name of matrix 
+//cbuffer cbPerObject : register(b0) {
+//    matrix gMVP;
+//}
+
+VertexOut main(VertexIn vin)
+{
+    VertexOut vout;
+    
+    vout.color = float4(vin.color, 1.0f);
+    vout.position = mul(float4(vin.position, 1.0f), ModelViewProjectionCB.MVP);
+    
+    return vout;
 }
