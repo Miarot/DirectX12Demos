6cb2584430b056eb353d536d25fea56f5e21f767
Miarot <zinchenkodan@gmail.com>
2023-06-25 15:01:15 +0300

    inc/SimpleGeoApp.h

--- src/SimpleGeoApp.cpp
+++ src/SimpleGeoApp.cpp
@@ -26,7 +26,9 @@ bool SimpleGeoApp::Initialize() {
 	BuildPipelineStateObject();
 
 	// for Sobel filter
-	CreateRTV_SRV_2DTexture();
+	m_FrameTextureRTVDescHeap = CreateDescriptorHeap(3, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, D3D12_DESCRIPTOR_HEAP_FLAG_NONE);
+	m_FrameTextureSRVDescHeap = CreateDescriptorHeap(3, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE);
+	UpdateFramesTextures();
 	BuildSobelRootSignature();
 	BuildSobelPipelineStateObject();
 
@@ -88,37 +90,51 @@ void SimpleGeoApp::OnRender() {
 	ComPtr<ID3D12GraphicsCommandList> commandList = m_DirectCommandQueue->GetCommandList();
 
 	auto backBuffer = m_BackBuffers[m_CurrentBackBufferIndex];
-	auto interimBackBuffer = m_InterimRTBuffers[m_CurrentBackBufferIndex];
+	auto frameTextureBuffer = m_FrameTexturesBuffers[m_CurrentBackBufferIndex];
 
-	CD3DX12_CPU_DESCRIPTOR_HANDLE rtv(
+	CD3DX12_CPU_DESCRIPTOR_HANDLE mainRTV(
 		m_BackBuffersDescHeap->GetCPUDescriptorHandleForHeapStart(),
 		m_CurrentBackBufferIndex, m_RTVDescSize
 	);
 
-	CD3DX12_CPU_DESCRIPTOR_HANDLE interimRTV(
-		m_InterimRTDescHeap->GetCPUDescriptorHandleForHeapStart(),
+	CD3DX12_CPU_DESCRIPTOR_HANDLE textureRTV(
+		m_FrameTextureRTVDescHeap->GetCPUDescriptorHandleForHeapStart(),
 		m_CurrentBackBufferIndex, m_RTVDescSize
 	);
 
+	CD3DX12_CPU_DESCRIPTOR_HANDLE geometryRTV;
+
+	if (m_IsSobelFilter) {
+		geometryRTV = textureRTV;
+	} else {
+		geometryRTV = mainRTV;
+	}
+
 	// clear RTs and DS buffer
 	{
-		CD3DX12_RESOURCE_BARRIER rtvBarrier = CD3DX12_RESOURCE_BARRIER::Transition(
+		CD3DX12_RESOURCE_BARRIER backBufferBarrier = CD3DX12_RESOURCE_BARRIER::Transition(
 			backBuffer.Get(),
 			D3D12_RESOURCE_STATE_PRESENT,
 			D3D12_RESOURCE_STATE_RENDER_TARGET
 		);
 
-		CD3DX12_RESOURCE_BARRIER interimRTVBarrier = CD3DX12_RESOURCE_BARRIER::Transition(
-			interimBackBuffer.Get(),
-			D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE,
-			D3D12_RESOURCE_STATE_RENDER_TARGET
-		);
+		if (m_IsSobelFilter) {
+			CD3DX12_RESOURCE_BARRIER frameTextureBufferBarrier = CD3DX12_RESOURCE_BARRIER::Transition(
+				frameTextureBuffer.Get(),
+				D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE,
+				D3D12_RESOURCE_STATE_RENDER_TARGET
+			);
 
-		D3D12_RESOURCE_BARRIER bariers[] = { rtvBarrier, interimRTVBarrier };
-		commandList->ResourceBarrier(2, bariers);
+			D3D12_RESOURCE_BARRIER bariers[] = { backBufferBarrier, frameTextureBufferBarrier };
+			commandList->ResourceBarrier(_countof(bariers), bariers);
 
-		commandList->ClearRenderTargetView(rtv, m_BackGroundColor, 0, NULL);
-		commandList->ClearRenderTargetView(interimRTV, m_BackGroundColor, 0, NULL);
+			commandList->ClearRenderTargetView(textureRTV, m_BackGroundColor, 0, NULL);
+		} else {
+			D3D12_RESOURCE_BARRIER bariers[] = { backBufferBarrier };
+			commandList->ResourceBarrier(_countof(bariers), bariers);
+		}
+
+		commandList->ClearRenderTargetView(mainRTV, m_BackGroundColor, 0, NULL);
 
 		commandList->ClearDepthStencilView(
 			m_DSVDescHeap->GetCPUDescriptorHandleForHeapStart(),
@@ -128,7 +144,7 @@ void SimpleGeoApp::OnRender() {
 		);
 	}
 
-	// draw to interim render target
+	// draw geometry
 	{
 		// set root signature and descripotr heaps
 		commandList->SetGraphicsRootSignature(m_RootSignature.Get());
@@ -153,7 +169,7 @@ void SimpleGeoApp::OnRender() {
 		commandList->RSSetViewports(1, &m_ViewPort);
 
 		// set Output Mergere Stage
-		commandList->OMSetRenderTargets(1, &interimRTV, FALSE, &m_DSVDescHeap->GetCPUDescriptorHandleForHeapStart());
+		commandList->OMSetRenderTargets(1, &geometryRTV, FALSE, &m_DSVDescHeap->GetCPUDescriptorHandleForHeapStart());
 
 		// set Input Asembler Stage
 		commandList->IASetVertexBuffers(0, 1, &m_BoxAndPiramidGeo.VertexBufferView());
@@ -191,22 +207,21 @@ void SimpleGeoApp::OnRender() {
 	}
 
 	// apply filter and draw to main render target
-	{
+	if (m_IsSobelFilter) {
 		CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(
-			interimBackBuffer.Get(),
+			frameTextureBuffer.Get(),
 			D3D12_RESOURCE_STATE_RENDER_TARGET,
 			D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE
 		);
 
 		commandList->ResourceBarrier(1, &barrier);
 
-
-		// unbind all resources of commandlist
+		// unbind all resources from pipeline
 		commandList->ClearState(NULL);
 
 		// set root signature and descripotr heaps
 		commandList->SetGraphicsRootSignature(m_SobelRootSignature.Get());
-		ID3D12DescriptorHeap* descriptorHeaps[] = { m_SRVDescHeap.Get() };
+		ID3D12DescriptorHeap* descriptorHeaps[] = { m_FrameTextureSRVDescHeap.Get() };
 		commandList->SetDescriptorHeaps(1, descriptorHeaps);
 
 		// set pso
@@ -217,14 +232,14 @@ void SimpleGeoApp::OnRender() {
 		commandList->RSSetViewports(1, &m_ViewPort);
 
 		// set Output Mergere Stage
-		commandList->OMSetRenderTargets(1, &rtv, FALSE, NULL);
+		commandList->OMSetRenderTargets(1, &mainRTV, FALSE, NULL);
 
 		// set Input Asembler Stage
 		commandList->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
 		// set root parameter
 		CD3DX12_GPU_DESCRIPTOR_HANDLE srv(
-			m_SRVDescHeap->GetGPUDescriptorHandleForHeapStart(),
+			m_FrameTextureSRVDescHeap->GetGPUDescriptorHandleForHeapStart(),
 			m_CurrentBackBufferIndex, m_CBDescSize
 		);
 		commandList->SetGraphicsRootDescriptorTable(0, srv);
@@ -255,7 +270,7 @@ void SimpleGeoApp::OnRender() {
 
 void SimpleGeoApp::OnResize() {
 	BaseApp::OnResize();
-	CreateRTV_SRV_2DTexture();
+	UpdateFramesTextures();
 }
 
 void SimpleGeoApp::OnKeyPressed(WPARAM wParam) {
@@ -278,6 +293,9 @@ void SimpleGeoApp::OnKeyPressed(WPARAM wParam) {
 
 		m_DirectCommandQueue->Flush();
 		ResizeDSBuffer();
+		break;
+	case '3':
+		m_IsSobelFilter = !m_IsSobelFilter;
 		break;
 	case 'W':
 	case 'S':
@@ -564,12 +582,9 @@ XMMATRIX SimpleGeoApp::GetProjectionMatrix() {
 	return projectionMatrix;
 }
 
-void SimpleGeoApp::CreateRTV_SRV_2DTexture() {
-	m_InterimRTDescHeap = CreateDescriptorHeap(3, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, D3D12_DESCRIPTOR_HEAP_FLAG_NONE);
-	m_SRVDescHeap = CreateDescriptorHeap(3, D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV, D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE);
-
-	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvDescHandle(m_InterimRTDescHeap->GetCPUDescriptorHandleForHeapStart());
-	CD3DX12_CPU_DESCRIPTOR_HANDLE srvDescHandle(m_SRVDescHeap->GetCPUDescriptorHandleForHeapStart());
+void SimpleGeoApp::UpdateFramesTextures() {
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvDescHandle(m_FrameTextureRTVDescHeap->GetCPUDescriptorHandleForHeapStart());
+	CD3DX12_CPU_DESCRIPTOR_HANDLE srvDescHandle(m_FrameTextureSRVDescHeap->GetCPUDescriptorHandleForHeapStart());
 
 	CD3DX12_RESOURCE_DESC resourceDesc = CD3DX12_RESOURCE_DESC::Tex2D(DXGI_FORMAT_R8G8B8A8_UNORM, m_ClientWidth, m_ClientHeight);
 	resourceDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET;
@@ -583,11 +598,11 @@ void SimpleGeoApp::CreateRTV_SRV_2DTexture() {
 			&resourceDesc,
 			D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE | D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE,
 			&clearValue,
-			IID_PPV_ARGS(&m_InterimRTBuffers[i])
+			IID_PPV_ARGS(&m_FrameTexturesBuffers[i])
 		));
 
-		m_Device->CreateRenderTargetView(m_InterimRTBuffers[i].Get(), nullptr, rtvDescHandle);
-		m_Device->CreateShaderResourceView(m_InterimRTBuffers[i].Get(), nullptr, srvDescHandle);
+		m_Device->CreateRenderTargetView(m_FrameTexturesBuffers[i].Get(), nullptr, rtvDescHandle);
+		m_Device->CreateShaderResourceView(m_FrameTexturesBuffers[i].Get(), nullptr, srvDescHandle);
 
 		rtvDescHandle.Offset(m_RTVDescSize);
 		srvDescHandle.Offset(m_CBDescSize);
